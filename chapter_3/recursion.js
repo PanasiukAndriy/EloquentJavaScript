9b62c332c655fca9fb0c249326f37579266ebf42
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-05-16 12:22:53 +0300

    recursion done

--- chapter_3/recursion.js
+++ chapter_3/recursion.js
@@ -10,13 +10,9 @@ function isEven(n)
 }
 
 console.log('0 ' + isEven(0));
-
 console.log('1 ' + isEven(1));
-
 console.log('5 ' + isEven(5));
 console.log('7 ' + isEven(7));
-
 console.log('10 ' + isEven(10));
-
 console.log('50 ' + isEven(50));
 console.log('75 ' + isEven(75));
