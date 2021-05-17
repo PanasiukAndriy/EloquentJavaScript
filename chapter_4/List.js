ca24dfbc065d8293fac313eab741811d06952e64
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-05-17 17:55:52 +0300

    add func GetValueByIndex

--- chapter_4/List.js
+++ chapter_4/List.js
@@ -26,11 +26,56 @@ function ListToArray(listParam) {
   return resultArray;
 }
 
+function Prepend(value, list) {
+  return new Element(value, list);
+}
+
+function GetValueByIndex(getIndex, list) {
+  let result;
+  let currentIndex = 0;
+  let found = false;
+  let endOfList = false;
+
+  do {
+    if (currentIndex == getIndex) {
+      result = list.value;
+      found = true;
+    }
+
+    if (list.ref == null) {
+      endOfList = true;
+    } else {
+      list = list.ref;
+      currentIndex++;
+    }
+  } while (!found && !endOfList);
+
+  return result;
+}
+
 let resultList = ArrayToList([1, 2, 3, 4, 5]);
 // console.log(JSON.stringify(result));
 // console.log(result);
 
 let resultArray = ListToArray(resultList);
 console.log("resultArray: " + resultArray);
+
+//--------------------------
+// let testValue = 7;
+// resultList = Prepend(testValue, resultList);
+// console.log(`after prepend ${testValue}`);
+// console.log(resultList);
+
+//-------------------
+console.log(resultList);
+
+let testIndex = 0;
+console.log(`index ${testIndex}: ` + GetValueByIndex(testIndex, resultList));
+
+testIndex = 2;
+console.log(`index ${testIndex}: ` + GetValueByIndex(testIndex, resultList));
+
+testIndex = 3;
+console.log(`index ${testIndex}: ` + GetValueByIndex(testIndex, resultList));
 
 console.log("done");
