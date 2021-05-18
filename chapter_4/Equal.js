05780f1c669dfb75979a4842da51c90e66a4aa0e
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-05-18 17:01:25 +0300

    looks like works )))

--- chapter_4/Equal.js
+++ chapter_4/Equal.js
@@ -1,45 +1,76 @@
 let classA = {
   fieldA: 1,
+  fieldC: {
+    val1: 1,
+    val2: 2,
+  },
   fieldB: 2,
   //fieldC: 4,
 };
 
 let classB = {
   fieldA: 1,
-  fieldB: 3,
-
-  //fieldD: 3,
+  fieldC: {
+    val1: 1,
+    val2: 3,
+  },
+  fieldB: 2,
+  //fieldC: 4,
 };
 
 function IsEqual(obj1, obj2) {
   let globalResult = true;
 
   if (obj1 === null && obj2 === null) {
+    console.log(`case 1`);
+    return (globalResult = true);
+  }
+
+  let obj1Keys = Object.keys(obj1);
+  obj1Keys.sort();
+  let obj2Keys = Object.keys(obj2);
+  obj2Keys.sort();
+
+  if (obj1Keys.length != obj2Keys.length) {
+    console.log(`case 2`);
     return (globalResult = false);
   }
-  console.log("length1 = " + Object.keys(obj1).length);
-  console.log("length2 = " + Object.keys(obj2).length);
-  if (Object.keys(obj1).length == Object.keys(obj2).length) {
-    let obj1Keys = Object.keys(obj1);
-    obj1Keys.sort();
-    let obj2Keys = Object.keys(obj2);
-    obj2Keys.sort();
-    console.log(obj1Keys.length);
-    console.log(obj2Keys.length);
 
-    for (let index = 0; index < obj1Keys.length; index++) {
-      //let result = IsEqual(obj1Keys[index], obj2Keys[index]);
+  for (let index = 0; index < obj1Keys.length; index++) {
+    if (obj1Keys[index] !== obj2Keys[index]) {
+      console.log(`case 3`);
+      return (globalResult = false);
+    }
+
+    if (
+      (typeof obj1[obj1Keys[index]] === "object" &&
+        typeof obj2[obj2Keys[index]] !== "object") ||
+      (typeof obj1[obj1Keys[index]] !== "object" &&
+        typeof obj2[obj2Keys[index]] === "object")
+    ) {
+      console.log(`case 4`);
+      return (globalResult = false);
+    }
+
+    if (typeof obj1[obj1Keys[index]] === "object") {
       if (
-        obj1Keys[index] !== obj2Keys[index] ||
-        obj1[obj1Keys[index]] !== obj2[obj2Keys[index]]
+        typeof obj1[obj1Keys[index]] === "object" &&
+        typeof obj2[obj2Keys[index]] === "object"
       ) {
+        let result = IsEqual(obj1[obj1Keys[index]], obj2[obj2Keys[index]]);
+        if (result === false) {
+          console.log(`case 5`);
+          return (globalResult = false);
+        }
+      }
+    } else {
+      if (obj1[obj1Keys[index]] !== obj2[obj2Keys[index]]) {
+        console.log(`case 6`);
         return (globalResult = false);
       }
     }
-  } else {
-    return (globalResult = false);
   }
-
+  console.log(`done`);
   return globalResult;
 }
 
@@ -52,3 +83,5 @@ console.log(r);
 // }
 
 //console.log("length = " + Object.keys(classA).length);
+
+//--------------
