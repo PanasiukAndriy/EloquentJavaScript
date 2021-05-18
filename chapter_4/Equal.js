26e9b3abc44a32389bbe0f86e672d2cc4b68fb02
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-05-18 15:59:58 +0300

    part of work on Equal (works if fields are not object)

--- chapter_4/Equal.js
+++ chapter_4/Equal.js
@@ -1,14 +1,14 @@
 let classA = {
   fieldA: 1,
   fieldB: 2,
-  fieldC: 4,
+  //fieldC: 4,
 };
 
 let classB = {
   fieldA: 1,
-  fieldB: 2,
+  fieldB: 3,
 
-  fieldD: 3,
+  //fieldD: 3,
 };
 
 function IsEqual(obj1, obj2) {
@@ -17,18 +17,22 @@ function IsEqual(obj1, obj2) {
   if (obj1 === null && obj2 === null) {
     return (globalResult = false);
   }
-
+  console.log("length1 = " + Object.keys(obj1).length);
+  console.log("length2 = " + Object.keys(obj2).length);
   if (Object.keys(obj1).length == Object.keys(obj2).length) {
-    let obj1Keys = Object.getOwnPropertyNames(obj1);
-    // obj1Keys.sort();
-    let obj2Keys = Object.getOwnPropertyNames(obj2);
-    // obj2Keys.sort();
-    // console.log(obj1Keys.length);
-    // console.log(obj2Keys.length);
+    let obj1Keys = Object.keys(obj1);
+    obj1Keys.sort();
+    let obj2Keys = Object.keys(obj2);
+    obj2Keys.sort();
+    console.log(obj1Keys.length);
+    console.log(obj2Keys.length);
 
     for (let index = 0; index < obj1Keys.length; index++) {
-      let result = IsEqual(obj1Keys[index], obj2Keys[index]);
-      if (result == false) {
+      //let result = IsEqual(obj1Keys[index], obj2Keys[index]);
+      if (
+        obj1Keys[index] !== obj2Keys[index] ||
+        obj1[obj1Keys[index]] !== obj2[obj2Keys[index]]
+      ) {
         return (globalResult = false);
       }
     }
@@ -36,8 +40,15 @@ function IsEqual(obj1, obj2) {
     return (globalResult = false);
   }
 
-  return result;
+  return globalResult;
 }
 
 let r = IsEqual(classA, classB);
 console.log(r);
+
+//------------------------------
+// for (const value of Object.keys(classA)) {
+//   console.log(`value = ` + classA[value]);
+// }
+
+//console.log("length = " + Object.keys(classA).length);
