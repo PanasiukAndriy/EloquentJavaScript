67526fe5237f489c76a5df8a1328c954dbda7950
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-06-29 15:57:44 +0300

    run robot

--- chapter_7_project_Robot/robot.js
+++ chapter_7_project_Robot/robot.js
@@ -1,75 +1,98 @@
 const roads = [
-    "Дом Алисы-Дом Боба",
-    "Дом Алисы-Почта",
-    "Дом Дарии-Дом Эрни",
-    "Дом Эрни-Дом Греты",
-    "Дом Греты-Магазин",
-    "Рынок-Почта",
-    "Рынок-Ратуша",
+  "Дом Алисы-Дом Боба",
+  "Дом Алисы-Почта",
+  "Дом Дарии-Дом Эрни",
+  "Дом Эрни-Дом Греты",
+  "Дом Греты-Магазин",
+  "Рынок-Почта",
+  "Рынок-Ратуша",
 
-    "Дом Алисы-Склад",
-    "Дом Боба-Ратуша",
-    "Дом Дарии-Ратуша",
-    "Дом Греты-Ферма",
-    "Рынок-Ферма",
-    "Рынок-Магазин",
-    "Магазин-Ратуша"
-]
+  "Дом Алисы-Склад",
+  "Дом Боба-Ратуша",
+  "Дом Дарии-Ратуша",
+  "Дом Греты-Ферма",
+  "Рынок-Ферма",
+  "Рынок-Магазин",
+  "Магазин-Ратуша",
+];
 
 function buildGraph(edges) {
-    let graph = Object.create(null);
-    function addEdge(from, to) {
-        if (graph[from] == null) {
-            graph[from] = [to];
-        }
-        else {
-            graph[from].push(to);
-        }
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
     }
+  }
 
-    for(let [from, to] of edges.map(r => r.split("-"))) {
-        addEdge(from, to);
-        addEdge(to, from);
-    }
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
 
-    return graph;
+  return graph;
 }
 
 const roadGraph = buildGraph(roads);
 
 class VillageState {
-    constructor(place , parcels) {
-        this.place = place;
-        this.parcels = parcels;
-    }
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
 
-    move(destination) {
-        if(!roadGraph[this.destination].includes(destination)) {
-            return this;
-        }
-        else {
-            let parcels = this.parcels.map(
-                p => {
-                    if(p.place != this.place) return p;
-                    return {place: destination, address: p.address}
-                }
-            ).filter(p =>  p.place != p.address);
-            
-            return new VillageState(destination, parcels);
-        }            
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+
+      return new VillageState(destination, parcels);
     }
+  }
 }
 
-function runRobot (state, robot, memory) {
-    for(let turn = 0;; turn++) {
-        if(state.parcels.parcels == 0) {
-            console.log(`Выполнено за ${turn} шагов`);
-            break;
-        }
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.parcels == 0) {
+      console.log(`Выполнено за ${turn} шагов`);
+      break;
+    }
 
-        let action = robot(state, memory);
-        state = state.move(action.direction);
-        memory = action.memory;
-        console.log(`Переход в  направлении ${action.direction}`);
-    }    
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Переход в  направлении ${action.direction}`);
+  }
 }
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+VillageState.random = function (parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (address == place);
+
+    parcels.push({ place, address });
+  }
+  return new VillageState("Почта", parcels);
+};
+
+runRobot(VillageState.random(), randomRobot);
