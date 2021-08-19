a8947531f351c45bbb66325f32d61232c1ca49b4
HOME-PC\Andrii <panasiukandriy87@gmail.com>
2021-08-19 17:27:09 +0300

    add findRoute and goalOrientedRobot

--- chapter_7_project_Robot/robot.js
+++ chapter_7_project_Robot/robot.js
@@ -120,4 +120,34 @@ function routeRobot(state, memory) {
   return { direction: memory[0], memory: memory.slice(1) };
 }
 
-runRobot(VillageState.random(), routeRobot, mailRoute);
+//дописав дві нижні функц. і поломав // to do
+function findRoute(graph, from, to) {
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, work } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some((w) => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({ place, parcels }, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) };
+}
+
+//first (before optimization)
+//runRobot(VillageState.random(), routeRobot, mailRoute);
+
+//second (after optimization)
+runRobot(VillageState.random(), goalOrientedRobot, findRoute);
